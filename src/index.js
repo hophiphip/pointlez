6bed27c3daa25ba7e6d67af80150f3bc42fc0bba
hophiphip <nighop@yandex.ru>
2022-02-04 01:02:24 +0400

    Module behaviour changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import Pointlez from './pointlez.js';
 
 // TODO: Add custom html element <replace></replace> | <replace data-from-string="..."></replace> | <replace data-from-file="..."></replace>
 
-Pointlez.start();
+//Pointlez.start();
 
 export default Pointlez;
