ff53a8881ef2746324829c082b19d799b00a8a7d
hophiphip <nighop@yandex.ru>
2022-02-04 01:14:50 +0400

    Added comments on imlementation impovement

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import Pointlez from './pointlez.js';
 // TODO: Add custom html element <replace></replace> | <replace data-from-string="..."></replace> | <replace data-from-file="..."></replace>
 
 //Pointlez.start();
+// windows.Pointlez = Pointlez;
 
 export default Pointlez;
